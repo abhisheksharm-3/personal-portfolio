69744c676cea4a21ede686362a96c922ebe05218
Abhishek <abhitiku2003@gmail.com>
2024-02-01 23:57:49 +0530

    minor change

--- src/components/BasePage.tsx
+++ src/components/BasePage.tsx
@@ -57,16 +57,16 @@ const BasePage = ({
       {children}
       <footer className="flex justify-between border-t-2 border-t-line text-[15px] text-fade-text">
         <div className="flex flex-grow">
-          <div className="border-r-2 border-r-line px-2 py-3 flex  items-center justify-center">
-            <span>find me in:</span>
+          <div className="border-r-2 border-r-line px-3 py-3 flex  items-center justify-center">
+            <span>find me on:</span>
           </div>
-          <div className="border-r-2 border-r-line px-2 py-3 flex  items-center justify-center cursor-pointer">
+          <div className="border-r-2 border-r-line px-3 py-3 flex  items-center justify-center cursor-pointer">
           <a href="https://www.linkedin.com/in/abhisheksharma20212025/" target="_blank">
             <RiLinkedinFill />
             </a>
           </div>
         </div>
-        <div className="border-l-2 border-l-line px-3 py-4 cursor-pointer">
+        <div className="border-l-2 border-l-line px-3 py-3 cursor-pointer">
         <a className="gap-2 flex  items-center justify-center" href="https://github.com/abhisheksharm-3" target="_blank">
         <span className="">@abhisheksharm-3</span><RiGithubFill/>
         </a>
