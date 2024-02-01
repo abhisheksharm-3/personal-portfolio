265b4ad17f6fd03d954719562a2633c76cc89a0e
Abhishek <abhitiku2003@gmail.com>
2024-02-01 23:53:58 +0530

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import BasePage from "@/components/BasePage"
+import BasePage from "@/components/BasePage";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "_hello - Abhishek Sharma",
+  description: "",
+};
+
 function LandingPage() {
   return (
     <BasePage className="" active="_hello">
-    Hi. I&apos; Abhishek Sharma  &gt;Another Front-End Developer
+      Hi. I&apos; Abhishek Sharma &gt;Another Front-End Developer
     </BasePage>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
