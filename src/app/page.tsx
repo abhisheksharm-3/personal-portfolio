004612e0ae776803db2c5684866bd9d9e66fc4f1
Abhishek <abhitiku2003@gmail.com>
2024-02-04 18:19:23 +0530

    made landing page for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ Code.theme = highlighterTheme;
 function LandingPage() {
   return (
     <BasePage className="" active="_hello">
-      <div className="flex items-center justify-around">
+      <div className="hidden lg:flex items-center justify-around">
         <div className="flex flex-col items-start justify-center gap-20">
           <div className="flex flex-col items-start justify-center gap-3">
             <span className="text-[16px]">&nbsp;Hi all. I am</span>
@@ -41,6 +41,30 @@ function LandingPage() {
               {snippet.Code}
             </Code>
           ))}
+        </div>
+      </div>
+      <div className="flex lg:hidden">
+        <div className="flex flex-col items-start justify-between px-4 w-screen py-10 gap-48">
+          <div className="flex flex-col gap-4 text-[#E5E9F0]">
+            <span className="text-[16px]">&nbsp;Hi all. I am</span>
+            <span className="text-6xl">Abhishek Sharma</span>
+            <span className="text-2xl text-green-text">
+              &gt; Front-End Developer
+            </span>
+          </div>
+
+          <div className="pt-6 flex flex-col gap-3">
+            <span className="text-fade-text">
+              &#x2F;&#x2F; see the portfolio code on my github
+            </span>
+            <span>
+              <span className="text-blue-text">const</span>{" "}
+              <span className="text-green-text">githubLink</span> ={" "}
+              <span className="text-skin-text">
+                &quot;https://github.com/abhisheksharm-3/personal-portfolio&quot;
+              </span>
+            </span>
+          </div>
         </div>
       </div>
     </BasePage>
