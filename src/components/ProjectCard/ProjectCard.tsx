5a4c2fa82db8e4f542d68125d139a560ddee955e
Abhishek <abhitiku2003@gmail.com>
2024-02-23 17:12:13 +0530

    project card minor change

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -42,19 +42,20 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   return (
     <>
       <div className="flex flex-col bg-[#011221] border-1 rounded-3xl lg:rounded-lg w-[370px] h-[315px] relative">
-        <div className="border-b-1 relative h-1/2">
+        {/* added showcase image to bg of upper div so as to avoid it overflowing to lower div and taking more space */}
+        <div className="border-b-1 relative h-1/2 bg-cover rounded-t-3xl lg:rounded-t-lg" style={{ backgroundImage: `url(${showcaseImage})` }}>  
           {/* Showcase Image */}
-          <Image
+          {/* <Image
             src={showcaseImage}
             height={125}
             width={370}
             alt={title}
             className="rounded-t-3xl lg:rounded-t-lg"
-          />
+          /> */}
           {/* Tech Stack Icon */}
           <TechIcon className="absolute top-0 right-0 transform -translate-x-1/2 translate-y-1/2" />
         </div>
-        <div className="flex flex-col w-full items-start gap-4 px-8 pt-4 justify-center h-1/2">
+        <div className="flex flex-col w-full items-start gap-4 px-8 justify-center h-1/2">
           <p className="text-fade-text">{description}</p>
           <Button asChild variant={"secondary"}>
             <Link href={link} target="_blank">view-project</Link>
