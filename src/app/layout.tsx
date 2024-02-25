5958e98d41f02389879977550f5d7d54065bb203
Abhishek <abhitiku2003@gmail.com>
2024-02-25 11:43:48 +0530

    added vercel analytisc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,8 +12,8 @@ export const metadata: Metadata = {
   description: "Portfolio Website for Abhishek Sharma",
   //add og protocol
   // <meta property="og:image" content="https://socialify.git.ci/abhisheksharm-3/personal-portfolio/png?description=1&descriptionEditable=Personal%20Portfolio%20Website%20for%20Abhishek%20Sharma&font=Jost&language=1&name=1&owner=1&pattern=Formal%20Invitation&stargazers=1&theme=Dark" />
-// <meta property="og:image:width" content="1280" />
-// <meta property="og:image:height" content="640" />
+  // <meta property="og:image:width" content="1280" />
+  // <meta property="og:image:height" content="640" />
 };
 
 export default function RootLayout({
@@ -22,7 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <SpeedInsights />
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
