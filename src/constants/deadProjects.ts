9ceb012c5bbf1fd9e05340b4e5dbf1ce4bcf9788
Abhishek Sharma <abhitiku2003@gmail.com>
2025-06-01 13:18:02 +0530

    Refactor code structure for improved readability and maintainability

--- src/constants/deadProjects.ts
+++ src/constants/deadProjects.ts
@@ -9,4 +9,14 @@ export const deadProjects = [
       lessonsLearned: "Always acquire domain name first and scope out enough time even for small projects.",
       resurrectionPotential: 10
     },
+    {
+      title: "Aventra",
+      showcaseImage: "/images/graveyard/aventra.png",
+      description: "An AI Powered Trip Planning Platform.",
+      techStack: ["Next.Js", "Appwrite", "FastAPI", "TailwindCSS"],
+      causeOfDeath: "Underestimated complexity and time required.",
+      lifetime: "April 2025 - May 2025",
+      lessonsLearned: "Ensure the project scope is well defined and manageable within the available time.",
+      resurrectionPotential: 60
+    },
   ];
